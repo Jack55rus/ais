7a1b85100bb7efaa04198b71978e8717f3578ed4
Evgeny Markin <evgenymarkin@Evgenys-MacBook-Air.local>
2021-03-23 01:59:55 +0300

    [WIP] added many new classes for points, spheres

--- ais/detector.py
+++ ais/detector.py
@@ -1,48 +1,119 @@
 import numpy as np
 from math import sqrt
+from abc import ABCMeta, abstractmethod
 
 
-class Detector:
-    # params: radius, coords
-    # methods: get params, create
-    def __init__(self, dim):
-        self.dim = dim  # dimensionality
-        self.coords = np.random.rand(self.dim)  # define random coords for the Detector
-        self.radius = np.random.random()  # do the same for its radius
-        self.eps = 1e-8  # how much detector radius is less than the minimum dist to ag
-        # add range for params later
+class Point:
+    def __init__(self, args: np.array):
+        self.coords = args
+        self.dim = len(self.coords)
 
-    def get_params(self):
-        return self.radius, self.coords
+    def get_dim(self) -> int:
+        return self.dim
 
-    def is_ag_inside(self, ag):
-        '''
-        Checks if an antigen is inside the detector
-        :param ag: np.array (dim,)
-        :return: True if point inside, False otherwise
-        '''
-        distance = np.linalg.norm(self.coords - ag)
+    def get_coords(self) -> np.array:
+        return self.coords
+
+    def calc_dist_to_other_point(self, other_point: 'Point') -> float:
+        return np.linalg.norm(self.coords - other_point)
+
+    # todo: method to calc dist to multiple points (think over some class for handling this stuff)
+    # def calc_dist_to_multiple_points(self, points: 'Point'):
+
+
+class HyperSphere:
+    def __init__(self, coords: Point, radius: float):
+        self.coords = coords
+        self.radius = radius
+
+    def get_radius(self) -> float:
+        return self.radius
+
+    def get_coords(self) -> Point:
+        return self.coords
+
+    def get_params(self) -> tuple[Point, float]:
+        return self.coords, self.radius
+
+    def is_point_inside(self, point: Point):
+        distance = self.coords.calc_dist_to_other_point(point)  # distance between center and some point
         return True if sqrt(distance) < self.radius else False
 
-    def ag_distance(self, ag: np.array) -> float:
-        '''
-        Computes distance between an antigen and a center of the detector
-        :param ag:
-        :return:
-        '''
-        return np.linalg.norm(self.coords - ag)
 
-    def min_ag_dist(self, ags):
-        unsqueezed_coords = np.expand_dims(self.coords, axis=0)
-        repeated_coords = np.repeat(unsqueezed_coords, ags.shape[0], axis=0)
-        min_dist = min(np.linalg.norm(ags - repeated_coords, axis=1))
-        return min_dist
+class RandomParamsGenerator(metaclass=ABCMeta):
+    ''' This is a base class for generating random parts of the algorithm '''
+    def __init__(self, *args):
+        pass
 
-    def change_radius(self, ags):
-        self.radius = self.min_ag_dist(ags) - self.eps
+    def __call__(self, *args):
+        pass
 
 
+class RandomPointGenerator(RandomParamsGenerator):
+    def __init__(self, dim):
+        RandomParamsGenerator.__init__(self)
+        self.dim = dim
+
+    def __call__(self) -> Point:
+        return Point(np.random.rand(self.dim).flatten())
+
+
+class RandomHyperSphereGenerator(RandomParamsGenerator):
+    def __init__(self, dim):
+        RandomParamsGenerator.__init__(self)
+        self.point = RandomPointGenerator(dim)
+        self.dim = dim
+
+    def __call__(self) -> HyperSphere:
+        center = self.point()
+        radius = np.random.random()
+        return HyperSphere(center, radius)
+
+
+class Detector(HyperSphere):
+    def __init__(self, center: Point, radius: float, eps: float = 1e-8):
+        HyperSphere.__init__(self, center, radius)
+        self.eps = eps
+
+    def change_radius(self, value: float):
+        self.radius += value
 
 
 
 
+# class Detector:
+#     def __init__(self, dim):
+#         self.dim = dim  # dimensionality
+#         self.coords = np.random.rand(self.dim)  # define random coords for the Detector
+#         self.radius = np.random.random()  # do the same for its radius
+#         self.eps = 1e-8  # how much detector radius is less than the minimum dist to ag
+#         # add range for params later
+#
+#     def get_params(self):
+#         return self.radius, self.coords
+#
+#     def is_ag_inside(self, ag):
+#         '''
+#         Checks if an antigen is inside the detector
+#         :param ag: np.array (dim,)
+#         :return: True if point inside, False otherwise
+#         '''
+#         distance = np.linalg.norm(self.coords - ag)
+#         return True if sqrt(distance) < self.radius else False
+#
+#     def ag_distance(self, ag: np.array) -> float:
+#         '''
+#         Computes distance between an antigen and a center of the detector
+#         :param ag:
+#         :return:
+#         '''
+#         return np.linalg.norm(self.coords - ag)
+#
+#     def min_ag_dist(self, ags):
+#         unsqueezed_coords = np.expand_dims(self.coords, axis=0)
+#         repeated_coords = np.repeat(unsqueezed_coords, ags.shape[0], axis=0)
+#         min_dist = min(np.linalg.norm(ags - repeated_coords, axis=1))
+#         return min_dist
+#
+#     def change_radius(self, ags):
+#         self.radius = self.min_ag_dist(ags) - self.eps
--- ais/immune_system.py
+++ ais/immune_system.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-from detector import Detector
+from detector import Detector, RandomPointGenerator, RandomHyperSphereGenerator, Point, HyperSphere
 import numpy as np
 
 
@@ -40,18 +40,46 @@ class NegativeSelection(AIS, ImmuneMemory):
         self.criterion = criterion
 
     def fit(self, X):
-        dim = X.shape[1]
-        for det_num in range(self.num_detectors):
-            det = Detector(dim=dim)
-            det.change_radius(X)
-            if det.get_params()[0] > 0:
-                self.expand(det)
+        # todo: implement things described above
+        '''
+        first, generate a random point
+        second, check if this point is inside an existing Detector
+        if inside -> go back to step 1,
+        otherwise calculate all distances between the candidate and all self's
+        then, find min value among them
+        take this value as a radius for candidate (optionally subtract eps)
+        add dR to the radius (it simulates type I error)
+        '''
+        pass
+        # dim = X.shape[1]
+        # detector_counter = 0
+        # while detector_counter < self.num_detectors:
 
-    def predict(self, X):
-        preds = np.zeros(shape=X.shape[0], dtype=np.uint8)
-        for i, ag in enumerate(X):
-            for detector in self.memory:
-                if detector.is_ag_inside(ag):  # if inside detector
-                    preds[i] = 1  # fraud
-                    continue
-        return preds
+
+
+
+# class NegativeSelection(AIS, ImmuneMemory):
+#
+#     def __init__(self, num_detectors=10, criterion='euclidean', init_memory=None):
+#         AIS.__init__(self)
+#         ImmuneMemory.__init__(self, init_memory)
+#         assert criterion in ['euclidean']
+#         self.num_detectors = num_detectors
+#         self.criterion = criterion
+#
+#     def fit(self, X):
+#         dim = X.shape[1]
+#         for det_num in range(self.num_detectors):
+#             det = Detector(dim=dim)
+#             det.change_radius(X)
+#             if det.get_params()[0] > 0:
+#                 self.expand(det)
+#
+#     def predict(self, X):
+#         preds = np.zeros(shape=X.shape[0], dtype=np.uint8)
+#         for i, ag in enumerate(X):
+#             for detector in self.memory:
+#                 if detector.is_ag_inside(ag):  # if inside detector
+#                     preds[i] = 1  # fraud
+#                     continue
+#         return preds
