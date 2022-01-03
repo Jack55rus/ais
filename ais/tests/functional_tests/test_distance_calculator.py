3df0275215a03e8b40fe01d0d06597d54c0132f6
Jack55rus <markin95evgeny@gmail.com>
2022-01-03 20:59:37 +0600

    [WIP] alc_min_dist_between_ags_and_point test

--- ais/tests/functional_tests/test_distance_calculator.py
+++ ais/tests/functional_tests/test_distance_calculator.py
@@ -10,9 +10,16 @@ def test_dist_between_points():
 
 
 def test_dist_between_ags_and_point():
-    ags = np.array([[1.0, 1.0], [2, 2.0]])
+    ags = np.array([[1.0, 1.0], [2.0, 2.0]])
     p = Point(coords=np.array([1.0, 2.0]))
     assert np.all(DistanceCalculator.calc_dist_between_ags_and_point(ags=ags, point=p) == np.array([1.0, 1.0]))
 
 
-# todo: calc_min_dist_between_ags_and_point test; then random params test
+def test_min_dist_between_ags_and_point():
+    ags = np.array([[1.0, 1.0, 0.0], [2.0, 0.0, 1.0], [0.5, 0.5, 0.5]])
+    p = Point(coords=np.array([1.0, 2.0, 0.5]))
+    # d1 = 1.118; d2 = 2.291; d3 = 1.581
+    assert round(DistanceCalculator.calc_min_dist_between_ags_and_point(ags=ags, point=p), 2) == 1.12
+
+
+# todo: then random params test
