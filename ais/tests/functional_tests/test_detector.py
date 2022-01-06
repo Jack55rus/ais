4a83f995b6a3c9397b846c0327cadc6ef20678fd
Jack55rus <markin95evgeny@gmail.com>
2022-01-06 17:56:58 +0600

    [WIP] detector eps test

--- ais/tests/functional_tests/test_detector.py
+++ ais/tests/functional_tests/test_detector.py
@@ -3,7 +3,12 @@ import numpy as np
 from ais.detector import Detector, Point
 
 
-def test_detector():
+def test_detector_change_radius():
     det = Detector(center=Point(coords=np.array([1.2, 1.6])), radius=0.3)
     det.change_radius(value=0.4)
     assert det.radius == 0.7
+
+
+def test_detector_eps():
+    det = Detector(center=Point(coords=np.array([1.2, 1.6])), radius=0.3, eps=0.5)
+    assert det.radius == 0.8
